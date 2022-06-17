69cf84e580c73fc38b7f226c807d31f046fd1b8d
Joey Milanes <joemilanes5@gmail.com>
2022-06-17 16:12:26 -0500

    home page addtion

--- packages/app/src/pages/home.tsx
+++ packages/app/src/pages/home.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Header } from "../components";
 
 const Home = () => {
-  return <Header text="Parsimony | The Best in Behavior Tracking" size="lg" />;
+  return (
+    <>
+      <Header text="Parsimony | The Best in Behavior Tracking" size="lg" />
+      <p>Things usually behave in the simplest way.</p>
+    </>
+  );
 };
 export default Home;
