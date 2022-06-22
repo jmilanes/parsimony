4863a41e3e5b654d3f0f8dc7293d6a64a984f7a3
Joey Milanes <joemilanes5@gmail.com>
2022-06-22 17:21:25 -0400

    fixing stuff

--- packages/server/src/collections/threads/resolvers.ts
+++ packages/server/src/collections/threads/resolvers.ts
@@ -3,6 +3,9 @@ import { ICreateResolverParams } from "@parsimony/types";
 export default ({ db, broadcast }: ICreateResolverParams) => ({
   Mutation: {
     createThread: async (_: any, { payload }: { payload: any }) => {
+      // TODO: Get direct mongo code out of the resolvers
+      // ** This should be createCollection("thread", payload)
+      // ** Make a simple object mock version to test that stays aligned or find a way to mock mongo :D
       const thread = new db.models.Thread({
         ...payload,
         messages: [payload.message]
