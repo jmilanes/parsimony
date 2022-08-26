49513106be23450e0cec9ef33c9e433164d27b55
Joey Milanes <joemilanes5@gmail.com>
2022-08-25 23:08:42 -0400

    server stuff

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -10,7 +10,7 @@ broadCastController.init();
 const { ApolloServer } = require("apollo-server");
 
 const CONNECTION_STRING = "mongodb://127.0.0.1:27017/parsimony";
-
+mongoose.Promise = global.Promise;
 const db = new DataBaseController(mongoose);
 db.connectDataBase(CONNECTION_STRING);
 db.createModels(models);
