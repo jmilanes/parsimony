7839a72c9ea5669cdf80602b83a6423f8a98ba09
Joey Milanes <joemilanes5@gmail.com>
2022-12-04 14:30:21 -0500

    server only

--- packages/server/src/database/broadCast.service.ts
+++ packages/server/src/database/broadCast.service.ts
@@ -24,7 +24,8 @@ export class BroadcastService {
       socket.on("close", () => console.log(`Connection Closed.`));
     });
 
-    this.webSocket = new WS(`ws://localhost:${this.port}`);
+    // this.webSocket = new WS(`ws://localhost:${this.port}`);
+    this.webSocket = new WS(`wss://api.parsimony.com:${this.port}`);
   };
 
   private _broadcastPayloadToClients = (clients: any[], payload: any) =>
