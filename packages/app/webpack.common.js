f8c5d88477374520bd4825e9439767107b36efe1
Joey Milanes <joemilanes5@gmail.com>
2023-03-05 16:41:17 -0500

    caching webpack

--- packages/app/webpack.common.js
+++ packages/app/webpack.common.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "bundle.js"
+    filename: "bundle.[contenthash].js"
   },
   module: {
     rules: [
