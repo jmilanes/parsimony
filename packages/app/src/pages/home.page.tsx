769e115b45ef7a576a6d0a4cb6716c35ee5d98d2
Joey Milanes <joemilanes5@gmail.com>
2023-03-27 21:56:23 -0400

    pub config

--- packages/app/src/pages/home.page.tsx
+++ packages/app/src/pages/home.page.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components";
 const Home = () => {
   return (
     <>
-      <Header text="Parsimony | The Best in Behavior Tracking!" size="md" />
+      <Header text="Parsimony | The Best in Behavior Tracking." size="md" />
       <p>Things usually behave in the simplest way.</p>
       <Header text="Principle of Parsimony:" size="md" />
       <p>
