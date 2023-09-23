cc0f8fc56311fcce354e84b527b16b2db55f7bee
Joey Milanes <joemilanes5@gmail.com>
2023-09-23 12:20:18 -0400

    ONE MO

--- packages/app/webpack.common.js
+++ packages/app/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+//com
 module.exports = {
   entry: "./src/index.tsx",
   output: {
